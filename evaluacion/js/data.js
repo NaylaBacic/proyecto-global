c5af5e65e97b605281b9639590c59d511b98ef14
NaylaBacic <naylabacic@hotmail.com>
2024-08-24 20:37:14 -0300

    Update data.js

--- evaluacion/js/data.js
+++ evaluacion/js/data.js
@@ -1,5 +1,5 @@
 export const products = [
-    { id: 1, title: 'Juguete para Perro', description: 'Juguete de peluche resistente.', price: 12.99 },
-    { id: 2, title: 'Comida para Perro', description: 'Comida balanceada para perros adultos.', price: 24.99 },
-    { id: 3, title: 'Collar para Perro', description: 'Collar ajustable y duradero.', price: 8.99 }
+    { id: 1, title: 'Juguete para Perro', description: 'Juguete de peluche resistente.', price: 1.000 },
+    { id: 2, title: 'Comida para Perro', description: 'Comida balanceada para perros adultos.', price: 20.000 },
+    { id: 3, title: 'Collar para Perro', description: 'Collar ajustable y duradero.', price: 8.000 }
 ];
